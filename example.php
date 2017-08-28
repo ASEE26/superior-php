15f2c936d8476c98a072ffb7a7532a7d3c3e7abe
mps01k <mike@asinetworkhn.com>
2017-08-28 11:53:23 -0600

    RPC

--- example.php
+++ example.php
@@ -7,7 +7,7 @@ $wallet = new Superior\Wallet();
 
 $destination1 = (object) [
     'amount' => '0.01',
-    'address' => '47Vmj6BXSRPax69cVdqVP5APVLkcxxjjXdcP9fJWZdNc5mEpn3fXQY1CFmJDvyUXzj2Fy9XafvUgMbW91ZoqwqmQ6RjbVtp'
+    'address' => '57Vmj6BXSRPax69cVdqVP5APVLkcxxjjXdcP9fJWZdNc5mEpn3fXQY1CFmJDvyUXzj2Fy9XafvUgMbW91ZoqwqmQ6RjbVtp'
 ];
 
 $options = [
